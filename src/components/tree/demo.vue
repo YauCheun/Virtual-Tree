c3b37cc90f480a09ddcdb214af7e4549c3c82e27
YauCheun <1272125039@qq.com>
2022-07-12 22:25:34 +0800

    feat：树结构拍平

--- src/components/tree/demo.vue
+++ src/components/tree/demo.vue
@@ -9,7 +9,7 @@
 import { defineComponent, ref } from "vue";
 import { TreeNodeOptions } from "./types";
 
-function recursion(path = "0", level = 3, h = 6): TreeNodeOptions[] {
+function recursion(path = "0", level = 3, h = 3): TreeNodeOptions[] {
   const list = [];
   for (let i = 0; i < h; i += 1) {
     const nodeKey = `${path}-${i}`;
@@ -17,6 +17,7 @@ function recursion(path = "0", level = 3, h = 6): TreeNodeOptions[] {
       nodeKey,
       name: nodeKey,
       children: [],
+      expanded: true,
     };
     if (level > 0) {
       treeNode.children = recursion(nodeKey, level - 1);
--- src/components/tree/index.tsx
+++ src/components/tree/index.tsx
@@ -1,16 +1,57 @@
 /* eslint-disable prettier/prettier */
-import { defineComponent, PropType } from "vue";
-import { TreeNodeOptions } from "./types";
+import { defineComponent, PropType, ref, watch } from "vue";
+import { RequiredTreeNodeOptions, TreeNodeOptions } from "./types";
+const flattenTree = (source: TreeNodeOptions[]): RequiredTreeNodeOptions[] => {
+  const result: RequiredTreeNodeOptions[] = [];
+  const recursion = (
+    list: TreeNodeOptions[],
+    level = 0,
+    parent: RequiredTreeNodeOptions | null = null
+  ): RequiredTreeNodeOptions[] => {
+    return list.map((item) => {
+      const node: RequiredTreeNodeOptions = {
+        ...item,
+        level,
+        loading: false,
+        disabled: item.disabled || false,
+        expanded: item.expanded || false,
+        selected: item.selected || false,
+        checked: item.checked || false,
+        hasChildren: item.hasChildren || false,
+        children: item.children || [],
+        parentKey: parent?.nodeKey || null,
+      };
+      result.push(node)
+      if (item.expanded && node.children.length) {
+        node.children = recursion(node.children, level + 1, node);
+      }
+      return node;
+    });
+  };
+  if (source.length) {
+    recursion(source);
+  }
+  return result;
+};
 export default defineComponent({
   name: "ATree",
   props: {
     source: {
       type: Array as PropType<TreeNodeOptions[]>,
-      default: ()=> []
-    }
+      default: () => [],
+    },
   },
-  setup(props,ctx) {
-    return ()=>{
+  setup(props, ctx) {
+    const flatList = ref<RequiredTreeNodeOptions[]>([]);
+    watch(
+      () => props.source,
+      (newVal) => {
+        flatList.value = flattenTree(newVal);
+        console.log(flatList.value,newVal);
+      },
+      { immediate: true }
+    );
+    return () => {
       return (
         <div class="ant-tree-wrap">
           <div class="ant-tree">
@@ -22,7 +63,7 @@ export default defineComponent({
             </div>
           </div>
         </div>
-      )
-    }
+      );
+    };
   },
 });
