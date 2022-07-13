11e978e41884a795e42a3c54c6d6a799e25892a9
YauCheun <1272125039@qq.com>
2022-07-13 22:57:39 +0800

    feat：节点收缩

--- src/components/tree/index.tsx
+++ src/components/tree/index.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable prettier/prettier */
 import { defineComponent, PropType, ref, watch } from "vue";
-import {cloneDeep} from 'lodash'
+import { cloneDeep } from "lodash";
 
-import { RequiredTreeNodeOptions, TreeNodeOptions } from "./types";
+import { nodeKey, RequiredTreeNodeOptions, TreeNodeOptions } from "./types";
 import "./index.scss";
 import ATreeNode from "./node";
 const flattenTree = (source: TreeNodeOptions[]): RequiredTreeNodeOptions[] => {
@@ -48,12 +48,12 @@ export default defineComponent({
   },
   setup(props, ctx) {
     const flatList = ref<RequiredTreeNodeOptions[]>([]);
-    const ExpandNode = (node: RequiredTreeNodeOptions)=>{
-      const trueChildren = cloneDeep(node.children) 
-      node.children = trueChildren.map(item=>{
+    const ExpandNode = (node: RequiredTreeNodeOptions) => {
+      const trueChildren = cloneDeep(node.children);
+      node.children = trueChildren.map((item) => {
         return {
           ...item,
-          level: item.level || node.level +1,
+          level: item.level || node.level + 1,
           loading: false,
           disabled: item.disabled || false,
           expanded: item.expanded || false,
@@ -63,25 +63,53 @@ export default defineComponent({
           hasChildren: item.hasChildren || false,
           children: item.children || [],
           parentKey: node.nodeKey || null,
-        }
-      })
-      const targetIndex = flatList.value.findIndex(i=>i.nodeKey === node.nodeKey)
-      if(targetIndex > -1){
-        flatList.value.splice(targetIndex+1,0,...(node.children as RequiredTreeNodeOptions[]))
+        };
+      });
+      const targetIndex = flatList.value.findIndex(
+        (i) => i.nodeKey === node.nodeKey
+      );
+      if (targetIndex > -1) {
+        flatList.value.splice(
+          targetIndex + 1,
+          0,
+          ...(node.children as RequiredTreeNodeOptions[])
+        );
       }
-    }
+    };
+    const collapseNode = (node: RequiredTreeNodeOptions) => {
+      const delKeys: nodeKey[] = [];
+      const recursion = (currentNode: RequiredTreeNodeOptions) => {
+        if (currentNode.children.length) {
+          currentNode.children.forEach((item) => {
+            delKeys.push(item.nodeKey);
+            if (item.expanded) {
+              item.expanded = false;
+              recursion(item as RequiredTreeNodeOptions);
+            }
+          });
+        }
+        if (delKeys.length) {
+          flatList.value = flatList.value.filter(
+            (i) => !delKeys.includes(i.nodeKey)
+          );
+        }
+      };
+      node.expanded
+      recursion(node);
+    };
     const handleToggleExpand = (node: RequiredTreeNodeOptions) => {
       node.expanded = !node.expanded;
       if (node.expanded) {
         // 展开
         // 有可能是用户第一次展开，没有把children处理成RequiredTreeNodeOptions类型
         if (node.children.length) {
-          ExpandNode(node)
+          ExpandNode(node);
         } else {
           // 懒加载
         }
       } else {
         // 收起
+        collapseNode(node);
       }
     };
     watch(
