335af65a0d8cd3aa131c0049168d75688570558c
YauCheun <1272125039@qq.com>
2022-08-08 07:42:25 +0800

    上传

--- src/lib-uses.ts
+++
@@ -1,7 +0,0 @@
-import { App } from "vue";
-import { VirList, VirTree } from "./components";
-const components = [VirList, VirTree];
-
-export default function (app: App) {
-  components.forEach((item) => app.component(item.name, item));
-}
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./assets/styles/index.scss";
-// import libUse from "./lib-uses";
-import VirTree from "./components/index";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-createApp(App).use(VirTree).use(ElementPlus).mount("#app");
